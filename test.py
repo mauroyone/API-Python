aeb24cd4eff2b601989fb23134f1e01cbece06c8
Mauro Yone <mauro.s.yoneitaya@exxonmobil.com>
2019-09-23 11:56:40 -0300

    Added timedate lib to print the raisetime in a more userfriendly way

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ learning to use the requests library to play with APIs
 import sys
 import requests
 import json
+from datetime import datetime
 
 def jprint(obj):
     # create a formatted string of the Python JSON object
@@ -16,11 +17,13 @@ def jprint(obj):
 
 if __name__ == '__main__':
 
+    '''
     response = requests.get('http://api.open-notify.org/astros.json')
 
     if response.status_code == 200:
         jprint(response.json())
-    
+    '''
+
     if len(sys.argv) > 2:
         parameters = {
             'lat': sys.argv[1],
@@ -30,4 +33,6 @@ if __name__ == '__main__':
         response = requests.get('http://api.open-notify.org/iss-pass.json', params = parameters)
 
         if response.status_code == 200:
-            jprint(response.json())
+            #jprint(response.json())
+            for d in response.json()['response']:
+                print(datetime.fromtimestamp(d['risetime']))
